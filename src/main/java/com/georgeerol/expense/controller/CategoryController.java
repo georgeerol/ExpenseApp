7006b611017d5f5796f8843406bcbfe6104a24e9
George Fouche <fouliex@gmail.com>
2019-11-02 13:25:09 -0700

    Add post delete put

--- src/main/java/com/georgeerol/expense/controller/CategoryController.java
+++ src/main/java/com/georgeerol/expense/controller/CategoryController.java
@@ -4,11 +4,11 @@ import com.georgeerol.expense.model.Category;
 import com.georgeerol.expense.repository.CategoryRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.Optional;
 
@@ -40,5 +40,25 @@ public class CategoryController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
-    
+    @PostMapping("/category")
+    ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) throws URISyntaxException {
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.created(new URI("/api/category" + result.getId())).body(result);
+
+    }
+
+    @PutMapping("/category/{id}")
+    ResponseEntity<Category> updateCategory(@Valid @RequestBody Category category) {
+        Category result = categoryRepository.save(category);
+        return ResponseEntity.ok().body(result);
+
+    }
+
+    @DeleteMapping("/category/{id}")
+    ResponseEntity<?> deleteCategory(@PathVariable Long id) {
+        categoryRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
 }
