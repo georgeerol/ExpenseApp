5e731e1cf902d36448004d1eb8d732fdde522ced
George Fouche <fouliex@gmail.com>
2019-11-03 16:38:46 -0800

    Add navbar to category and expenses components

--- app/src/App.js
+++ app/src/App.js
@@ -1,11 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    
-  );
+import React, { Component } from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Category from "./Category";
+import Home from "./Home";
+import Expenses from "./Expenses";
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/categories" exact={true} component={Category} />
+          <Route path="/expenses" exact={true} component={Expenses} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
