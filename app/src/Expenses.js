73e4ff304389937e813f335e452c22986d582c0f
George Fouche <fouliex@gmail.com>
2019-11-03 16:38:46 -0800

    Add navbar to category and expenses components

---
+++ app/src/Expenses.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import AppNav from "./AppNav";
+class Expenses extends Component {
+  render() {
+    return (
+      <div>
+        <AppNav />
+        <h2>Expenses</h2>
+      </div>
+    );
+  }
+}
+export default Expenses;
