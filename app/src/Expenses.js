189355a5b24b069c0fe9f9bbfafae7c09d84f5cd
George Fouche <fouliex@gmail.com>
2019-11-05 08:30:08 -0800

    Format Expense form

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -1,66 +1,71 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import AppNav from "./AppNav";
 import DatePicker from "react-datepicker";
 import "./App.css";
 import "react-datepicker/dist/react-datepicker.css";
-import {Container, Input, Button, Label, Form, FormGroup} from "reactstrap";
-import {Link} from 'react-router-dom';
+import { Container, Input, Button, Label, Form, FormGroup } from "reactstrap";
+import { Link } from "react-router-dom";
 
 class Expenses extends Component {
-    state = {};
+  state = {
+    date: new Date()
+  };
 
-    handleChange;
+  handleChange;
 
-    render() {
-        return (
-            <div>
-                <AppNav/>
-                <Container>
-                    <Form>
-                        <FormGroup>
-                            <label for="title">Title</label>
-                            <input
-                                type="text"
-                                name="title"
-                                id="title"
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <label for="category">Category</label>
-                            <input
-                                type="text"
-                                name="category"
-                                id="category"
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <label for="expenseDate">Expense Date</label>
-                            <DatePicker selected={this.state.date}onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                        <FormGroup>
-                            <label for="location">Location</label>
-                            <input
-                                type="text"
-                                name="location"
-                                id="location"
-                                onChange={this.handleChange}
-                            />
-                        </FormGroup>
-                        <FormGroup></FormGroup>
-                        <Button color="primary" type="submit">
-                            Save
-                        </Button>{" "}
-                        <Button color="secondary" tag={Link} to="/categories">
-                            Cancel
-                        </Button>
-                    </Form>
-                </Container>
+  render() {
+    const title = <h3>Add Expense</h3>;
+    return (
+      <div>
+        <AppNav />
+        <Container>
+          {title}
+          <Form onSubmit={this.handleSubmit}>
+            <FormGroup>
+              <Label for="title">Title</Label>
+              <Input
+                type="text"
+                name="title"
+                id="title"
+                onChange={this.handleChange}
+                autoComplete="name"
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="category">Category</Label>
+              <Input
+                type="text"
+                name="category"
+                id="category"
+                onChange={this.handleChange}
+              />
+            </FormGroup>
+            <FormGroup>
+              <Label for="city"> Date</Label>
+              <DatePicker
+                selected={this.state.date}
+                onChange={this.handleDateChange}
+              />
+            </FormGroup>
+            <div className="row">
+              <FormGroup className="col-md-4 mb-3">
+                <Label for="location">Location</Label>
+                <Input type="text" name="location" id="location" />
+              </FormGroup>
             </div>
-        );
-    }
+            <FormGroup>
+              <Button color="primary" type="submit">
+                Save
+              </Button>{" "}
+              <Button color="secondary" tag={Link} to="/categories">
+                Cancel
+              </Button>
+            </FormGroup>
+          </Form>
+        </Container>
+      </div>
+    );
+  }
 }
 
 export default Expenses;
