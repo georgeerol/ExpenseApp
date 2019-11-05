5cc361396a8a498c565618796e6fec6b207d013f
George Fouche <fouliex@gmail.com>
2019-11-05 07:43:39 -0800

    Add Form to Expenses class

--- app/src/Expenses.js
+++ app/src/Expenses.js
@@ -1,13 +1,66 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import AppNav from "./AppNav";
+import DatePicker from "react-datepicker";
+import "./App.css";
+import "react-datepicker/dist/react-datepicker.css";
+import {Container, Input, Button, Label, Form, FormGroup} from "reactstrap";
+import {Link} from 'react-router-dom';
+
 class Expenses extends Component {
-  render() {
-    return (
-      <div>
-        <AppNav />
-        <h2>Expenses</h2>
-      </div>
-    );
-  }
+    state = {};
+
+    handleChange;
+
+    render() {
+        return (
+            <div>
+                <AppNav/>
+                <Container>
+                    <Form>
+                        <FormGroup>
+                            <label for="title">Title</label>
+                            <input
+                                type="text"
+                                name="title"
+                                id="title"
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <label for="category">Category</label>
+                            <input
+                                type="text"
+                                name="category"
+                                id="category"
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <label for="expenseDate">Expense Date</label>
+                            <DatePicker selected={this.state.date}onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <FormGroup>
+                            <label for="location">Location</label>
+                            <input
+                                type="text"
+                                name="location"
+                                id="location"
+                                onChange={this.handleChange}
+                            />
+                        </FormGroup>
+                        <FormGroup></FormGroup>
+                        <Button color="primary" type="submit">
+                            Save
+                        </Button>{" "}
+                        <Button color="secondary" tag={Link} to="/categories">
+                            Cancel
+                        </Button>
+                    </Form>
+                </Container>
+            </div>
+        );
+    }
 }
+
 export default Expenses;
